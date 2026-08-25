ba5cd3ca97d717ee04264978856b5876834b20bc
manishsaurabhinfosys <manish@saurabhinfosys.com>
2026-08-25 10:52:36 +0530

    Update HELPERS_Head constant with new value

--- js/helpers.js
+++ js/helpers.js
@@ -1,5 +1,5 @@
 (function () {
-  const HELPERS_Head = "";
+  const HELPERS_Head = "phs_378abb7a8432abe5af13c4c3d7c39dfe8809e987d5791e81";
   const LIST_API_URL =
     "https://meow-service-test.flutterclone.com/api/public/helpers/list?nopaginate=1";
   const INFO_API_URL =
